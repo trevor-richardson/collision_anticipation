79c929c967af0be9698854aaa7dc151e2eb8601b
trevor-richardson <trevorwoodsrichardson@gmail.com>
2018-02-12 17:55:42 -0700

    built data generator

--- machine_learning/train_anticipation.py
+++ machine_learning/train_anticipation.py
@@ -13,11 +13,27 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from anticipation_model import Custom_Spatial_Temporal_Anticipation_NN
 import argparse
+config = configparser.ConfigParser()
+config.read('../config.ini')
+
+base_dir = config['DEFAULT']['BASE_DIR']
+
+from anticipation_model import Custom_Spatial_Temporal_Anticipation_NN
+
+'''Argparse Variables for Dynamic Experimentation'''
 
 
+'''Training'''
 
+
+'''Testing/Validation'''
+
+
+'''Helper Function'''
+
+
+'''Initialize experiment'''
 def main():
     print("Starting program")
     model = Custom_Spatial_Temporal_Anticipation_NN((3, 64, 64), (32, 24, 16), (5, 5), 2, .3, 1)
