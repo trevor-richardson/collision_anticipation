5ab563825d409b58f51d4ae119a91a744c6cbfba
trevor-richardson <trevorwoodsrichardson@gmail.com>
2018-02-13 21:34:04 -0700

    visualize outputs update3

--- machine_learning/visualizer.py
+++ machine_learning/visualizer.py
@@ -77,4 +77,6 @@ class VisualizeActivations(object):
                 xlabel_i.set_fontsize(0.0)
             for xlabel_i in frame1.axes.get_yticklabels():
                 xlabel_i.set_fontsize(0.0)
+            mng = plt.get_current_fig_manager()
+            mng.resize(*mng.window.maxsize())
             plt.show()
